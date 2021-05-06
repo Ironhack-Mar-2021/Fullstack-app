b3fbf5010bd9e3d66a8dec8cf26b5ee18c5b8cbf
Cynthia Navarro <cynthianavarro93@hotmail.com>
2021-05-06 10:42:28 -0400

    backend stuff

--- back-end/index.js
+++ back-end/index.js
@@ -4,9 +4,23 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+const Blog = require("./blogmodel.js");
+
+mongoose
+  .connect('mongodb+srv://cynthia:cynthia@cluster0.xgsgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
 
 app.get("/home", (req, res) => console.log(res));
 
-app.post("/", (req, res) => console.log(req.body));
+app.post("/", (req, res) => {
+    console.log(req.body)
+    Blog.create(req.body)
+});
 
 app.listen(5000, () => console.log("Listening on port 5000"));
