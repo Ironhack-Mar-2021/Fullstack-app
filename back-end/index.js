c74ba5f97722964ab2cadf670fb871b81570296e
davidmedero <david.gregory.medero@gmail.com>
2021-05-06 10:11:16 -0400

    david finished

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express()
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/home', (req, res) => console.log(res))
+app.get("/home", (req, res) => console.log(res));
 
-app.listen(5000, () => console.log("Listening on port 5000"))
+app.post("/", (req, res) => console.log(req.body));
+
+app.listen(5000, () => console.log("Listening on port 5000"));
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { useState, useEffect } from "react";
-
+import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
 
   function handleChange(e) {
-    setMessage(e.target.value)
-    console.log(message)
+    setMessage(e.target.value);
+    console.log(message);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post("http://localhost:5000", { message });
   }
 
   return (
     <div className="App">
       <h1>Class Blog</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} />
-        <button type='submit' >Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
